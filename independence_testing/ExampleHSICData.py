793fe378b26fe190058d65ca38e827f9de8501e6
sejdino <dino.sejdinovic@gmail.com>
2016-07-15 18:59:15 +0100

    more comments

--- independence_testing/ExampleHSICData.py
+++ independence_testing/ExampleHSICData.py
@@ -17,8 +17,7 @@ from independence_testing.HSICBlockTestObject import HSICBlockTestObject
 Given a data set data_x and data_y, we wish to test the independence between the two. 
 Both data_x and data_y should have the number of observations being the number of rows.  
 As an example, we simulate some data here.
-First, we need to specify the kernels for X and Y. Here, we use Gaussian Kernel with Median heuristic for both. But, 
-we will use a temporary setup for the bandwidth.  
+
 '''
 
 num_samples = 10000
@@ -26,7 +25,7 @@ dim = 20
 data_x, data_y = SimDataGen.LargeScale(num_samples, dim)
 
 '''
-use Gaussian kernels -- default value of the width parameter is 1.0
+First, we need to specify the kernels for X and Y. We will use Gaussian kernels -- default value of the width parameter is 1.0
 the widths can be either kept fixed or set to a median heuristic based on the data when running a test
 '''
 kernelX=GaussianKernel()
@@ -37,7 +36,7 @@ data_generator = None
 #HSIC Spectral Test using random Fourier features, as specified by rff = True
 myspectralobject = HSICSpectralTestObject(num_samples, data_generator, kernelX, kernelY, 
                 kernel_width_x=True,kernel_width_y=True,
-                 rff=True, num_rfx=20, num_rfy=20, num_nullsims=1000)
+                rff=True, num_rfx=20, num_rfy=20, num_nullsims=1000)
 pvalue,_ = myspectralobject.compute_pvalue(data_x, data_y)
 
 print "Spectral test p-value:", pvalue
