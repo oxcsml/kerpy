f52f459286ffc2d5b63b29cb99836f79f274e58c
Leon Law <ho.law@spc.ox.ac.uk>
2018-02-16 16:16:53 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kerpy',
+    version='0.1.0',
+    url='https://github.com/oxmlcs/kerpy',
+    packages=find_packages(),
+    description='Code for kernel methods',
+    license='MIT'
+)
